00a3fe2612d22985871b078b9d02cb6af78af195
Hardik Desai <124128351+hardikd-zignuts@users.noreply.github.com>
2023-03-31 17:32:01 +0530

    refactor: :recycle: Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import Navbar from '@/src/components/navbar'
+import '@/styles/main.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  )
 }
